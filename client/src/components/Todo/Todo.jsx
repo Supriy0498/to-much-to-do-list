1181aadc7ff0286fe328a5912f171e76d9ad7217
Supriy Jinturkar <supriyjinturkar@gmail.com>
2021-05-20 12:27:56 +0530

    added update todo functionality to react

--- client/src/components/Todo/Todo.jsx
+++ client/src/components/Todo/Todo.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Todo.css"
 import axios from "axios"
 
 export default function Todo(props) {
 
-
+    const [isEdit,setIsEdit] = useState(false);
+    const [task,setTask] = useState(props.todo.task);
     const url = "/api/todos/"+props.todo._id;
 
     async function handleDelete() {
@@ -17,10 +18,40 @@ export default function Todo(props) {
             .catch(err=>console.log(err));
     }
 
+    function updateTodo() {
+        axios.put(url,{task:task})
+            .then(res=>{
+                console.log(res);
+                setIsEdit(false);
+                props.updateTodos();
+            })
+            .catch(err=>console.log(err));
+    }
+
+    function handleCancel() {
+        setIsEdit(false);
+    }
+
+    function handleOnEdit() {
+        setIsEdit(true)
+    }
+
+    function handleChange(e) {
+        setTask(e.target.value)
+    }
+
     return (
         <div className="todo">
-            {props.todo.task}
-            <button className="del-btn" onClick={handleDelete}><i class="fa fa-trash fa-lg"></i></button>
+            {!isEdit && task}
+            {isEdit && <input type="text" 
+            defaultValue={props.todo.task}
+            onChange={handleChange}
+            autoFocus
+            /> }
+            {!isEdit && <button className="del-btn" onClick={handleDelete}><i class="fa fa-trash fa-lg"></i></button>}
+            {!isEdit && <button className="edit-btn" onClick={handleOnEdit}><i class="fa fa-edit fa-lg"></i></button>}
+            {isEdit && <button className="cross-btn" onClick={handleCancel}><i class="fa fa-times-circle fa-2x"></i></button>}
+            {isEdit && <button className="check-btn" onClick={updateTodo}><i class="fa fa-check-circle fa-2x"></i></button>}
         </div>
     )
 }
